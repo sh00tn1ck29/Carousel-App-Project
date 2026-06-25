2af2fe7844a4edc4369f56000a844c4a724532e0
sh00tn1ck29 <maksimsavrygin21@gmail.com>
2026-06-25 20:11:22 +0200

    Update index.js

--- src/gateways/index.js
+++ src/gateways/index.js
@@ -1,10 +1,14 @@
 const apiLink =
-  'https://developer.nps.gov/api/v1/multimedia/galleries?limit=10&start=2&q=national%20park&api_key=HdIJfDqu1pskl6oqkLEhWLTxFujYosuSMN1ghkbm';
+  'https://developer.nps.gov/api/v1/multimedia/galleries?start=2&q=national%20park&api_key=HdIJfDqu1pskl6oqkLEhWLTxFujYosuSMN1ghkbm';
 
 export const fetchParks = () => {
   return fetch(apiLink)
     .then((response) => {
-      return response.ok ? response.json() : Promise.reject('Ошибка запроса');
+      return response.ok ? response.json() : Promise.reject('Request error');
     })
-    .then((data) => data.data);
+    .then((data) => data.data)
+    .catch((error) => {
+      console.error(error);
+      alert('Failed to load national parks data. Please try again later.');
+    });
 };
